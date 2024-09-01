deab345a77d31ab85fd71d342b02c81ace298391
Tereta Alexander <atereta@gomage.com>
2024-09-01 21:03:38 +0300

    Library bug fixing 0.0.50

--- src/Cli/Make.php
+++ src/Cli/Make.php
@@ -5,9 +5,27 @@ namespace Framework\Cli\Cli;
 use Framework\Application\Manager as ApplicationManager;
 use Framework\Cli\Interface\Controller;
 use Framework\Cli\Symbol;
-use Framework\Database\Abstract\Model as AbstractModel;
 use Exception;
 
+/**
+ * ···························WWW.TERETA.DEV······························
+ * ·······································································
+ * : _____                        _                     _                :
+ * :|_   _|   ___   _ __    ___  | |_    __ _        __| |   ___  __   __:
+ * :  | |    / _ \ | '__|  / _ \ | __|  / _` |      / _` |  / _ \ \ \ / /:
+ * :  | |   |  __/ | |    |  __/ | |_  | (_| |  _  | (_| | |  __/  \ V / :
+ * :  |_|    \___| |_|     \___|  \__|  \__,_| (_)  \__,_|  \___|   \_/  :
+ * ·······································································
+ * ·······································································
+ *
+ * @class Framework\Cli\Cli\Make
+ * @package Framework\Cli\Cli
+ * @link https://tereta.dev
+ * @since 2020-2024
+ * @license   http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ * @author Tereta Alexander <tereta.alexander@gmail.com>
+ * @copyright 2020-2024 Tereta Alexander
+ */
 class Make implements Controller
 {
     /**
